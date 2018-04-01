fb0f7aa216d43f8ee4488977b0d0aa31cd39066b
cy <icaiyu@163.com>
2018-04-01 16:25:05 +0800

    odify the test.py

--- typing/templates/static/vocabulary/test.py
+++ typing/templates/static/vocabulary/test.py
@@ -9,10 +9,13 @@ words = [ x.split(" ")[0] for x in stk]
 print words[-1]
 d = collections.defaultdict(list)
 
-for w in words:
-	if w[0].upper() in "UVWXYZ": d["UVWXYZ"].append(w)
-	else: d[w[0].upper()].append(w)
-#shuffle(d['A'])
+	for w in words:
+		if w[0].upper() in "UVWXYZ": d["UVWXYZ"].append(w)
+		elif w[0].upper() in "GH": d["GH"].append(w)
+		elif w[0].upper() in "JKL": d["JKL"].append(w)
+		elif w[0].upper() in "NO": d["NO"].append(w)
+		elif w[0].upper() in "QR": d["QR"].append(w)
+		else: d[w[0].upper()].append(w)
 
 
 for x in sorted(d.keys()):
