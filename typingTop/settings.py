a88f292e587e2f6a59bf8829759d87d5285c4077
cy <icaiyu@163.com>
2018-04-10 21:02:47 +0800

    add img

--- typingTop/settings.py
+++ typingTop/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'p2d2cn63be%(#wk3a-9!axjf#%fi&w)q)g^43yjroufk6!6g_^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*',]
 
